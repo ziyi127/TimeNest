ec84140043b14a792505a120be14aa124e5063f8
ziyi127 <ziyihed@outlook.com>
2025-08-15 17:57:02 +0800

    hhh

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,3 +1,21 @@
 """
 插件系统包初始化文件
 """
+
+from .plugin_interface import PluginInterface
+from .plugin_manager import PluginManager
+from .course_plugin_interface import CoursePluginInterface
+from .schedule_plugin_interface import SchedulePluginInterface
+from .ui_plugin_interface import UIPluginInterface
+from .data_plugin_interface import DataPluginInterface
+from .course_reminder_plugin import CourseReminderPlugin
+
+__all__ = [
+    "PluginInterface",
+    "PluginManager",
+    "CoursePluginInterface",
+    "SchedulePluginInterface",
+    "UIPluginInterface",
+    "DataPluginInterface",
+    "CourseReminderPlugin"
+]
--- plugins/data_plugin_interface.py
+++ plugins/data_plugin_interface.py
@@ -3,7 +3,7 @@
 """
 
 from abc import ABC, abstractmethod
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 from plugins.plugin_interface import PluginInterface
 
 
